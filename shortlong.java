7cab8b2b2bd011a511254533f222543aeb2e7596
NguyenTruong <truongphucnguyen.dev@gmail.com>
2024-07-25 14:54:56 +0700

    rn cau1.java -> shortlong.java

---
+++ shortlong.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class shortlong{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input:");
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        String shortestLastWord = words[0];
+        String longestFirstWord = words[0];
+        for (String word : words) {
+            if (word.length() > longestFirstWord.length()) {
+                longestFirstWord = word;
+            }
+        }
+        for (String word : words) {
+            if (word.length() <= shortestLastWord.length()) {
+                shortestLastWord = word;
+            }
+        }
+        System.out.println("Output:" + shortestLastWord + " " + longestFirstWord);
+        scanner.close();
+    }
+}
